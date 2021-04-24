0a7969eb29df845fa27692c6cb559147e29ba850
Максим <massering@yandex.ru>
2021-04-24 21:38:11 +0700

    Улучшение html

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from flask import render_template, redirect, abort
 
 from app.data.db_session import create_session
 from app.data.__all_models import News
+from app.data.__all_models import Award
 
 
 # помимо 404 будет обрабатываться ещё и попытка перейти на
@@ -19,4 +20,18 @@ def handle_404(error=""):
 def home_page():
     db_sess = create_session()
     news = db_sess.query(News).all()
-    return render_template('home_page.html', title='Главная', news_list=news)
+    return render_template('home_page.html', title='Главная', news_list=news).replace('992', '600')
+
+
+@app.route('/news')
+def news_page():
+    db_sess = create_session()
+    news = db_sess.query(News).all()
+    return render_template('news_page.html', title='Новости', news_list=news)
+
+
+@app.route('/awards')
+def awards_page():
+    db_sess = create_session()
+    awards = db_sess.query(Award).all()
+    return render_template('awards_page.html', title='Достижения', awards_list=awards)
